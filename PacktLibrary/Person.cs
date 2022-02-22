df145e2813999263e0d537ff05a032a0ca1e9301
Jérôme JACOB <jerome56jj@hotmail.com>
2022-02-22 08:50:04 +0100

    Add project files.

---
+++ PacktLibrary/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace Packt.Shared
+{
+    public class Person : object
+    {
+        // fields
+        public string Name;
+        public DateTime DateOfBirth;
+    }
+}
---
+++ PeopleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿using Packt.Shared;
+using static System.Console;
+
+Person bob = new();
+WriteLine(bob.ToString());
