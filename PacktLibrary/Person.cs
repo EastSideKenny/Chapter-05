18636b22164b8c1f6e720ba55d03a58e6324c6c2
Jerome Jacob <Jerome56jj@hotmail.com>
2022-02-22 20:48:59 +0100

    parameters, ref, out

--- PacktLibrary/Person.cs
+++ PacktLibrary/Person.cs
@@ -58,5 +58,52 @@ namespace Packt.Shared
             return (Name: "Apples", Number: 5);
         }
 
+        public string SayHello()
+        {
+            return $"{Name} says 'Hello!'";
+        }
+
+        public string SayHello(string name)
+        {
+            return $"{Name} says 'Hello {name}!'";
+        }
+
+        // deconstructors
+        public void Deconstruct(out string name, out DateTime dob)
+        {
+            name = Name;
+            dob = DateOfBirth;
+        }
+
+        public void Deconstruct(out string name, out DateTime dob, out WondersOfTheAncientWorld fav)
+        {
+            name = Name;
+            dob = DateOfBirth;
+            fav = FavoriteAncientWonder;
+        }
+
+        public string OptionalParameters(
+            string command = "Run!",
+            double number = 0.0,
+            bool active = true)
+        {
+            return string.Format(
+                format: "command is {0}, number is {1}, active is {2}",
+                arg0: command,
+                arg1: number,
+                arg2: active);
+        }
+
+        public void PassingParameters(int x, ref int y, out int z)
+        {
+            // out parameters cannot have a default
+            // AND must be initialized inside the method
+            z = 99;
+
+            // increment each parameter
+            x++;
+            y++;
+            z++;
+        }
     }
 }
