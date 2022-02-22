df5d150e653e0aa62f66ce970877921f702423a1
Jérôme JACOB <jerome56jj@hotmail.com>
2022-02-22 08:56:48 +0100

    updated main method

--- PeopleApp/Program.cs
+++ PeopleApp/Program.cs
@@ -1,5 +1,11 @@
 ﻿using Packt.Shared;
+using System;
 using static System.Console;
 
 Person bob = new();
-WriteLine(bob.ToString());
+bob.Name = "Bob Smith";
+bob.DateOfBirth = new DateTime(1965, 12, 22);
+
+WriteLine(format: "{0} was born on {1:dddd, d MMMM yyyy}",
+    arg0: bob.Name,
+    arg1: bob.DateOfBirth);
