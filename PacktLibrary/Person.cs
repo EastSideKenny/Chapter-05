13e4e555c193409520ca8f4ffb6aa5ce9a1a78e8
Jérôme JACOB <jerome56jj@hotmail.com>
2022-02-22 13:02:28 +0100

    Enums

--- PacktLibrary/Person.cs
+++ PacktLibrary/Person.cs
@@ -10,5 +10,7 @@ namespace Packt.Shared
         // fields
         public string Name;
         public DateTime DateOfBirth;
+        public WondersOfTheAncientWorld FavoriteAncientWonder;
+        public WondersOfTheAncientWorld BucketList;
     }
 }
